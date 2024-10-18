dae44148f6c19b690da45491c6c8014eeeee06d3
Dolcythareja <156478380+Dolcythareja@users.noreply.github.com>
2024-10-18 05:53:04 -0400

    Added changeBackgroundColor function

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -3,4 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const redColorSlider = document.getElementById("red");
     const blueColorSlider = document.getElementById("blue");
     const greenColorSlider = document.getElementById("green");
+
+    function changeBackgroundColor() {
+        // Collecting values from Sliders
+        const red = redColorSlider.value;
+        const blue = blueColorSlider.value;
+        const green = greenColorSlider.value;
+
+        // Updating the background color 
+        document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
+
+
+
+
+    
 });
