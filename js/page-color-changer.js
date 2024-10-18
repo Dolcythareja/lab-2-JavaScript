2d4962165fdb69ff1c5be7ac319f8af5f2749861
Dolcythareja <156478380+Dolcythareja@users.noreply.github.com>
2024-10-18 05:49:13 -0400

    Added slider element references (red, blue, green)

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+
+    const redColorSlider = document.getElementById("red");
+    const blueColorSlider = document.getElementById("blue");
+    const greenColorSlider = document.getElementById("green");
 });
