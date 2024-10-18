4c9f50a74318823b96f039d539e50de3243c89b0
Dolcythareja <156478380+Dolcythareja@users.noreply.github.com>
2024-10-18 06:01:13 -0400

    Added leftover comments

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,9 +1,11 @@
+//Script runs after the DOM has fully loaded.
 document.addEventListener("DOMContentLoaded", function() {
-
+    // Declare and initialize some constants for the input slider elements, the page body
     const redColorSlider = document.getElementById("red");
     const blueColorSlider = document.getElementById("blue");
     const greenColorSlider = document.getElementById("green");
-
+    
+    // Building a function to collect the values from the constants and change the background color
     function changeBackgroundColor() {
         // Collecting values from Sliders
         const red = redColorSlider.value;
